b8f482fbe533626b452d5894ad4e2097dacd96c1
Augusto Melo <augustinho.melo@hotmail.com>
2017-06-06 11:05:31 -0300

    Atividade 2 Terminada

---
+++ Assets/Scripts/CutSceneEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEvents : MonoBehaviour {
+
+
+    public void CameraAcabou() {
+        CutSceneController.instancia.ProximaCamera();
+
+
+
+    }
+
+	}
+
